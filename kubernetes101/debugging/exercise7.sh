75326c137f5add8e2cdc9125605b97d2a8198e27
Andres Guisado <andresguisado1@gmail.com>
2018-08-29 12:21:50 +0100

    Changing Debugging excercise 7

--- kubernetes101/debugging/exercise7.sh
+++ kubernetes101/debugging/exercise7.sh
@@ -3,6 +3,6 @@ sleep 1
 ls
 kubectl delete -f exceeding-resource.yml
 kubectl delete -f resource-quota.yml
-kubectl apply-f pod-quota.yml
-kubectl apply-f exceeding-pod.yml
+kubectl apply -f pod-quota.yml
+kubectl apply -f exceeding-pod.yml
 clear
